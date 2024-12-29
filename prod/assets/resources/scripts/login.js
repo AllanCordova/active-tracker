a0b6b895f04c1d1809836721954962883f8d9ef1
AllanCordova <allan.2024@alunos.utfpr.edu.br>
2024-12-29 14:01:48 -0300

    atualizado mantimento do login e novos links desktop

--- prod/assets/resources/scripts/login.js
+++ prod/assets/resources/scripts/login.js
@@ -3,22 +3,40 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
   
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
+    const nome = document.getElementById('name').value.trim();
     let erro = document.getElementById('loginMessage');
 
-    if (!email || !password) {
-        erro.style.display = 'block'
+    if (!email || !password || !nome) {
+        erro.style.display = 'block';
         return;
     }
   
     if (!(password == 'utfpr')) {
-        erro.style.display = 'block'
-        return
+        erro.style.display = 'block';
+        return;
     }
-
-    alert('Login enviado com sucesso!');
   
-    // Fecha o modal após validação
+    // esconde a senha caso escrita errada
+    erro.style.display = 'none';
+
+    // Obtém o elemento do modal
     const modalElement = document.getElementById('exampleModal');
-    const modal = bootstrap.Modal.getInstance(modalElement); // Obtém a instância do modal aberta no momento
+
+    // Obtém a instância do modal Bootstrap
+    const modal = bootstrap.Modal.getInstance(modalElement);
     modal.hide();
+
+    // Simula o sucesso do login
+    setTimeout(() => {
+        // Altera o conteúdo do modal dinamicamente
+        const modalBody = document.getElementById('modalBody');
+        modalBody.innerHTML = `
+            <div class="text-center">
+                <h3 class="poppins-medium success p-2">Welcome back!</h3>
+                <p class="poppins-medium">${nome}</p>
+            </div>
+        `;
+        modal.show();
+    }, 500)
+
 });
